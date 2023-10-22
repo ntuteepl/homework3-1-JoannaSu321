8c957a455147fa4f0d122752a0cc3ef6b63cb480
JoannaSu321 <146065302+JoannaSu321@users.noreply.github.com>
2023-10-22 15:56:55 +0800

    Update hw3-2.c

--- hw3-2.c
+++ hw3-2.c
@@ -1,1 +1,35 @@
 
+#include <stdio.h>
+main()
+{
+    int s1, d1, s2, d2, s3, d3, car=0;
+    scanf("%d%d%d%d%d%d", &s1, &d1, &s2, &d2, &s3, &d3);
+    if (s1==s2==s3){
+        car=3;
+    }
+    else if(((s1<=s2&&s2<d1)||(s2<s1&&s1<=d2))&&((s1<=s3&&s3<d1)||(s3<s1&&s3<=d3))&&((s3<=s2&&s2<d3)||(s1<s3&&s3<=d1))){
+    car+=3;
+    }
+    else if(((s1<=s2&&s2<d1)||(s2<s1&&s1<=d2))&&((s1<=s3&&s3<d1)||(s3<s1&&s1<=d3))){
+        car+=2;
+    }
+    else if(((s1<=s3&&s3<d1)||(s3<s1&&s1<=d3))&&((s3<=s2&&s2<d3)||(s1<s3&&s3<=d1))){
+        car+=2;
+    }
+    else if(((s3<=s2&&s2<d3)||(s1<s3&&s3<=d1))&&((s1<=s2&&s2<d1)||(s2<s1&&s1<=d2))){
+        car+=2;
+    }
+    else if((s1<=s2&&s2<d1)||(s2<s1&&s1<=d2)){
+        car++;
+    }
+    else if((s1<=s3&&s3<d1)||(s3<s1&&s1<=d3)){
+        car++;
+    }
+    else if((s3<=s2&&s2<d3)||(s1<s3&&s3<=d1)){
+        car++;
+    }
+    else{
+        car=+1;
+    }
+    printf("%d\n", car);
+}
