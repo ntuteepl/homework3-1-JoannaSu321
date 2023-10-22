f97f055400d24286ea44b6dad64a13ceabffa248
JoannaSu321 <146065302+JoannaSu321@users.noreply.github.com>
2023-10-22 17:12:38 +0800

    Update hw3-2.c

--- hw3-2.c
+++ hw3-2.c
@@ -11,25 +11,26 @@ main()
     car+=3;
     }
     else if(((s1<=s2&&s2<d1)||(s2<s1&&s1<=d2))&&((s1<=s3&&s3<d1)||(s3<s1&&s1<=d3))){
-        car+=2;
+        car+=3;
     }
     else if(((s1<=s3&&s3<d1)||(s3<s1&&s1<=d3))&&((s3<=s2&&s2<d3)||(s1<s3&&s3<=d1))){
-        car+=2;
+        car+=3;
     }
     else if(((s3<=s2&&s2<d3)||(s1<s3&&s3<=d1))&&((s1<=s2&&s2<d1)||(s2<s1&&s1<=d2))){
-        car+=2;
+        car+=3;
     }
     else if((s1<=s2&&s2<d1)||(s2<s1&&s1<=d2)){
-        car++;
+        car+=2;
     }
     else if((s1<=s3&&s3<d1)||(s3<s1&&s1<=d3)){
-        car++;
+        car+=2;
     }
     else if((s3<=s2&&s2<d3)||(s1<s3&&s3<=d1)){
-        car++;
+        car+=2;
     }
     else{
         car=+1;
     }
     printf("%d\n", car);
+
 }
